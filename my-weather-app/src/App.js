efa3eed93aea63c63d8cbfa824078f6f1753fb9a
tylerking406 <tylerking406@gmail.com>
2024-11-13 18:45:33 +0200

    get user's location and only fetch data when lat and long are not empty

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,12 +1,44 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-import MyWeather from './myWeather';
+import Weather from './components/weather';
 
-function App() {
+export default function App() {
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Get the user's location
+    navigator.geolocation.getCurrentPosition(position => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  }, []);
+
+  useEffect(() => {
+    // Fetch data only when lat and long have values
+    const fetchData = async () => {
+      if (lat && long) {
+        await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+            setData(result);
+            console.log(result);
+          })
+          .catch(error => console.error('Error fetching data:', error));
+      }
+    };
+
+    fetchData();
+  }, [lat, long]);
+
   return (
     <div className="App">
-     <MyWeather/>
+      {(data && data.main) ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div>Loading weather data...</div>
+      )}
     </div>
   );
 }
-
-export default App;
